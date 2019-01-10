b675226e842e2bd869df2fc60b0a0f269c8001e1
halzhan <halzhan163@163.com>
2019-01-10 20:10:13 +0800

    Site updated: 2019-01-10 20:10:13

---
+++ js/randomBack.js
@@ -0,0 +1,27 @@
+(function() {
+    var GRADIENTS = [
+        "linear-gradient(to left top, lightgrey 0%, lightgrey 1%, #e0e0e0 26%, #efefef 48%, #d9d9d9 75%, #bcbcbc 100%)",
+        "linear-gradient(to left top, #50cc7f 0%, #f5d100 100%)",
+        "linear-gradient(to left top, #f9d423 0%, #ff4e50 100%)",
+        "linear-gradient(to left top, #B6CEE8 0%, #F578DC 100%)",
+        "linear-gradient(to left top, #69EACB 0%, #EACCF8 48%, #6654F1 100%)",
+        "linear-gradient(to left top, #231557 0%, #44107A 29%, #FF1361 67%, #FFF800 100%)"
+    ];
+    $.fn.extend({
+        randomBack: function(images) {
+            // if (images && images.length) {
+            var idx = Math.floor(Math.random() * GRADIENTS.length);
+            // var image = images[idx] || {};
+            // var url = image.url || "";
+
+            if (GRADIENTS[idx]) {
+                this.css({
+                    backgroundImage: GRADIENTS[idx],
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                });
+            }
+            // }
+        }
+    });
+})();
